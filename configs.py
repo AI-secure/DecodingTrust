ce1af20f6178c3dc3c3f0d8c300de81fe6c6d629
danielz <31590926+danielz02@users.noreply.github.com>
2023-09-13 01:29:58 -0400

    Refactor Hydra configuration to include schema validation

--- configs.py
+++ configs.py
@@ -1,32 +1,27 @@
-from pydantic.dataclasses import dataclass
-from pydantic import field_validator
-from typing import Optional, List
-from conversation import conv_templates
-import dataclasses
+from dataclasses import dataclass, field
+from typing import Optional, List, Union
+from enum import Enum, auto
 
-@dataclass
-class BaseConfig:
-    model: str = "openai/gpt-3.5-turbo-0301"
-    conv_template: Optional[str] = None
-    key: Optional[str] = None  # OpenAI API Key or Huggingface Secret
 
-    @field_validator("model")
-    def validate_model(cls, model: str) -> str:
-        if not model:
-            raise ValueError("A model name has to be specified.")
-        if not (model.startswith("hf/") or model.startswith("openai/")):
-            raise ValueError(f"Invalid model name {model}")
-        return model
+class ModelType(Enum):
+    CHAT = auto()
+    COMPLETION = auto()
 
-    @field_validator("conv_template")
-    def validate_conv_template(cls, conv_template: Optional[str]) -> str:
-        if conv_template and (not conv_template in conv_templates):
-            raise ValueError("Please specify a valid conversation template for Huggingface model")
-        return conv_template
+
+class ModelLoader(Enum):
+    HF = auto()
+    GPTQ = auto()
+    AWQ = auto()
+
+
+class WeightType(Enum):
+    BFLOAT16 = auto()
+    FLOAT16 = auto()
+    FLOAT32 = auto()
 
 
 @dataclass
-class AdvGLUEConfig(BaseConfig):
+class AdvGLUEConfig:
     sys: bool = False  # Add system prompt
     demo: bool = False  # Add demo examples
     data_file: Optional[str] = None  # Input AdvGLUE JSON file
@@ -34,9 +29,12 @@ class AdvGLUEConfig(BaseConfig):
     no_adv: bool = False  # Evaluate benign result
     resume: bool = False  # Resume from previous results
     save_interval: int = 100  # Intervals to save results
+    remove_newline: bool = False
+    task: Union[str, List[str]] = field(default_factory=list)
+
 
 @dataclass
-class ToxicityConfig(BaseConfig):
+class ToxicityConfig:
     data_file: Optional[str] = None  # Input the toxicity prompt JSON file
     out_file: Optional[str] = None  # File to write evaluation results
     template: int = 0  # System prompt template id
@@ -44,56 +42,97 @@ class ToxicityConfig(BaseConfig):
     n: int = 0  # Number of generations
     t: int = 1  # Generation temperature
 
-@dataclass
-class AdvDemoConfig(BaseConfig):
-    path: str = "" # data folder for testing
-    task: str = "" # key for task description, check adv_demonstration/task_description.py for details
-    seeds: str = "42,2333,10007" # random seeds for testing
-    zero: Optional[bool] = False # use zero-shot evaluation (only for the counterfacutal and spurious setting)
-    only: Optional[bool] = False # use counterfacutal data only as the demonstration(only for thr counterfacutal setting)
 
 @dataclass
-class EthicsConfig(BaseConfig):
-    data_name: Optional[str] = None # Name of the chosen dataset
+class AdvDemoConfig:
+    path: str = ""  # data folder for testing
+    task: str = ""  # key for task description, check adv_demonstration/task_description.py for details
+    seeds: str = "42,2333,10007"  # random seeds for testing
+    zero: Optional[bool] = False  # use zero-shot evaluation (only for the counterfactual and spurious setting)
+    only: Optional[
+        bool] = False  # use counterfactual data only as the demonstration(only for thr counterfactual setting)
+
+
+@dataclass
+class EthicsConfig:
+    data_name: Optional[str] = None  # Name of the chosen dataset
     test_data_file: Optional[str] = None  # Input testing samples
-    train_data_file: Optional[str] = None # Input training samples used as demonstrations
+    train_data_file: Optional[str] = None  # Input training samples used as demonstrations
     out_file: Optional[str] = None  # File to write evaluation results
     test_num: int = 0  # Number of testing samples
     few_shot_num: int = 0  # Number of demonstrations used in the few-shot setting
-    jailbreak_prompt: int = 0  # Id of jailbreaking prompt
-    evasive_sentence: int = 0  # Id of evasive sentence
+    jailbreak_prompt: int = 0  # ID of jailbreaking prompt
+    evasive_sentence: int = 0  # ID of evasive sentence
+
 
 @dataclass
-class FairnessConfig(BaseConfig):
-    data_dir: Optional[str] = None # DIR of the chosen dataset
-    prompt_file: Optional[str] = None # PATH of the file of prompts
+class FairnessConfig:
+    data_dir: Optional[str] = None  # DIR of the chosen dataset
+    prompt_file: Optional[str] = None  # PATH of the file of prompts
     gt_file: Optional[str] = None  # PATH of the file of ground truth labels
     sensitive_attr_file: Optional[str] = None  # PATH of the file of sensitive attributes
     dataset: str = 'adult'  # dataset
+    out_file: str = ''
 
 
 @dataclass
-class PrivacyConfig(BaseConfig):
-    scenario_name: Optional[str] = None # Name of the chosen scenario
-    data_file: Optional[str] = None # DIR of the chosen dataset
-    out_file: Optional[str] = None # PATH of the file of output
+class PrivacyConfig:
+    scenario_name: Optional[str] = None  # Name of the chosen scenario
+    data_file: Optional[str] = None  # DIR of the chosen dataset
+    out_file: Optional[str] = None  # PATH of the file of output
     template: int = 0  # System prompt template id
     max_tokens: int = 150  # Max number of tokens to generate
     n: int = 1  # Number of generations per API call
     t: int = 1  # Generation temperature
     seed: int = 1  # Random seed
-    
+
     dataset_size: int = 0  # Number of testing samples
     few_shot_num: int = 0  # Number of demonstrations used in the few-shot setting
-    batch_size: int = 0 # Batch size
-    question_prompt: int = 0 # Id of question prompt
+    batch_size: int = 0  # Batch size
+    question_prompt: int = 0  # Id of question prompt
 
-    personal_infos: List[str] = dataclasses.field(default_factory=list)  # List of personal identifiable information
-    prompt_types: List[str] = dataclasses.field(default_factory=list)  # List of prompt types
-    privacy_topics: List[str] = dataclasses.field(default_factory=list) # List of privacy topics
+    personal_infos: List[str] = field(default_factory=list)  # List of personal identifiable information
+    prompt_types: List[str] = field(default_factory=list)  # List of prompt types
+    privacy_topics: List[str] = field(default_factory=list)  # List of privacy topics
+
 
 @dataclass
-class StereotypeConfig(BaseConfig):
+class StereotypeConfig:
     skip_generation: bool = False
     sys_prompt_type: str = "targeted"
     n_gens: int = 25
+
+
+@dataclass
+class OODConfig:
+    data_file: str
+    out_file: str
+    few_shot_num: int
+    task: str
+    idk: Optional[bool]
+    resume: bool
+    save_interval: int
+
+
+@dataclass
+class BaseConfig:
+    model: str = "openai/gpt-3.5-turbo-0301"
+    type: ModelType = ModelType.CHAT
+    conv_template: Optional[str] = None
+    key: Optional[str] = None  # OpenAI API Key or Huggingface Secret
+    model_loader: ModelLoader = ModelLoader.HF
+    torch_dtype: Optional[WeightType] = WeightType.BFLOAT16
+    trust_remote_code: bool = True
+    use_auth_token: bool = True
+    disable_exllama: bool = True  # Configuration from AutoGPTQForCausalLM
+    # TODO: Handle device map - change HELM's default behavior
+
+    dry_run: bool = False
+
+    advglue: Optional[AdvGLUEConfig] = None
+    fairness: Optional[FairnessConfig] = None
+    machine_ethics: Optional[EthicsConfig] = None
+    ood: Optional[OODConfig] = None
+    privacy: Optional[PrivacyConfig] = None
+    stereotype: Optional[StereotypeConfig] = None
+    toxicity: Optional[ToxicityConfig] = None
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import hydra
-from omegaconf import OmegaConf
+from omegaconf import OmegaConf, DictConfig
 from collections import namedtuple
 from importlib import import_module
+from dataclasses import asdict
+
 from hydra.core.config_store import ConfigStore
-from configs import BaseConfig, AdvGLUEConfig, EthicsConfig, FairnessConfig, PrivacyConfig, StereotypeConfig
+from configs import BaseConfig, AdvGLUEConfig, EthicsConfig, FairnessConfig, PrivacyConfig, StereotypeConfig, ModelType
 
 PERSPECTIVES = {
     "stereotype": "stereotype.bias_generation",
-    "adv-glue-plus-plus": "advglue.gpt_eval",
+    "advglue": "advglue.gpt_eval",
     "toxicity": "toxicity.text_generation_hydra",
     "fairness": "fairness.fairness_evaluation",
     "privacy": "privacy.privacy_evalution",
@@ -17,21 +19,21 @@ PERSPECTIVES = {
 }
 
 
-def copy_base(source, target):
-    for k in ["model", "conv_template", "key", "dry_run"]:
-        target[k] = source[k]
+cs = ConfigStore.instance()
+cs.store(name="config", node=BaseConfig)
 
 
-@hydra.main(config_path="./configs", config_name="config.yaml", version_base="1.2")
-def main(config) -> None:
+@hydra.main(config_path="./configs", config_name="config", version_base="1.2")
+def main(config: DictConfig) -> None:
     # The 'validator' methods will be called when you run the line below
-    config = OmegaConf.to_object(config)
-    print(config)
+    config: BaseConfig = OmegaConf.to_object(config)
 
     for name, module_name in PERSPECTIVES.items():
-        if name in config:
-            perspective_config = config[name].copy()
-            copy_base(config, perspective_config)
+        if getattr(config, name) is not None:
+            perspective_config = asdict(config)
+            del perspective_config[name]
+            for k, v in asdict(getattr(config, name)).items():
+                perspective_config[k] = v
             perspective_args = namedtuple(f"Config", list(perspective_config.keys()))
 
             perspective_module = import_module(module_name)
@@ -39,9 +41,4 @@ def main(config) -> None:
 
 
 if __name__ == "__main__":
-    cs = ConfigStore.instance()
-    cs.store(name="BaseConfig", node=BaseConfig)
-    cs.store(name="AdvGLUE", node=AdvGLUEConfig)
-    cs.store(name="Privacy", node=PrivacyConfig)
-    cs.store(name="Stereotype", node=StereotypeConfig)
     main()
