1545cd94f08207f54af24efa596e09142644d28e
danielz <31590926+danielz02@users.noreply.github.com>
2023-10-11 21:53:36 -0400

    Add missing comma in multi-run

--- scripts/multi-run.sh
+++ scripts/multi-run.sh
@@ -7,7 +7,7 @@ command="dt-run --config-name slurm_config --multirun +model_config=autogptq,hf"
 for folder in "${folders[@]}"; do
     # get all config names without the .yaml extension
     configs=$(ls src/dt/configs/${folder}/*.yaml | xargs -n 1 basename | sed 's/.yaml//' | tr '\n' ',' | sed 's/,$//')
-    eval "${command} ${folder}=${configs}"
+    eval "${command} +${folder}=${configs}"
 done
 
 # run the constructed command
