65d73c595cf0b93ca6816a2debdc75e9ed7fa278
acphile <hpei4@illinois.edu>
2023-10-19 14:24:02 -0700

    refactor aggregate

--- src/dt/perspectives/adv_demonstration/adv_demonstration_hydra.py
+++ src/dt/perspectives/adv_demonstration/adv_demonstration_hydra.py
@@ -1,14 +1,20 @@
 from .test_cf_hydra import main as counterfactual_main
 from .test_sc_hydra import main as spurious_main
 from .test_bkd_hydra import main as backdoor_main
-
+from .aggregate_score import counterfactual_score, spurious_score, backdoor_score, aggregate_all
 
 def main(args):
     if args.adv_demonstration.path.startswith("./data/adv_demonstration/counterfactual/"):
-        return counterfactual_main(args)
+        counterfactual_main(args)
     elif args.adv_demonstration.path.startswith("./data/adv_demonstration/spurious/"):
-        return spurious_main(args)
+        spurious_main(args)
     elif args.adv_demonstration.path.startswith("./data/adv_demonstration/backdoor/"):
-        return backdoor_main(args)
+        backdoor_main(args)
     else:
         raise ValueError
+    score = {}
+    model = args.model_config.model
+    score["counterfactual"] = counterfactual_score(model)
+    score["spurious"] = spurious_score(model)
+    score["backdoor"] = backdoor_score(model)
+    final = aggregate_all(score)   
