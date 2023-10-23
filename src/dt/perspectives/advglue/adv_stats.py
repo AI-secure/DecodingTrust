2d2b91fb505901fd10b080f4295c68f9181c1032
danielz <31590926+danielz02@users.noreply.github.com>
2023-10-23 15:16:56 -0400

    Automate model reading in AdvGLUE result aggregation

--- src/dt/perspectives/advglue/adv_stats.py
+++ src/dt/perspectives/advglue/adv_stats.py
@@ -1,64 +1,60 @@
 import os, json
+from glob import glob
 import numpy as np, pandas as pd
 
 
 RESULT_DIR = "./results/adv-glue-plus-plus"
-ORGANIZATIONS = ["openai", "hf", "together"]  # TODO: refactor when we have the central database
+ORGANIZATIONS = ["openai", "hf", "together", "anthropic"]  # TODO: refactor when we have the central database
 BASE_MODELS = ["alpaca", "vicuna", "stable-vicuna"]
 
 
 def main():
-    target_models = [
-        "llama-2-13b-chat_magnitude_semistruct@0.5_2to4",
-        "vita-group/llama-2-13b-chat_sparsegpt_semistruct@0.5_2to4",
-        "vita-group/vicuna-13b-v1.3_magnitude_semistruct@0.5_2to4",
-        "vita-group/vicuna-13b-v1.3_sparsegpt_semistruct@0.5_2to4"
-    ]
+    benign_files = glob(os.path.join(RESULT_DIR, "**", "*benign*.json"), recursive=True)
+    target_models = [os.path.dirname(x) for x in benign_files]
 
     df = {
         "BaseModel": [], "TargetModel": [], "Transferability": [], "Accuracy": [], "AccuracyNoRefusal": [],
         "Task": [], "RR+NE": [], "TaskDataCount": []
     }
 
-    for organization in ORGANIZATIONS:
-        base_dir = os.path.join(RESULT_DIR, organization)
+    base_dir = ""
+    for target_model in target_models:
+        for base_model in BASE_MODELS:
+            print(os.path.join(base_dir, target_model, f"{base_model}-demo.json"))
+            if not os.path.exists(os.path.join(base_dir, target_model, f"{base_model}-demo.json")):
+                continue
+            with open(os.path.join(base_dir, target_model, f"{base_model}-demo.json")) as f:
+                j = json.load(f)
+                for task in j.keys():
+                    df["BaseModel"].append(base_model)
+                    df["TargetModel"].append(target_model.removeprefix(RESULT_DIR))
+                    df["Task"].append(task)
+                    df["TaskDataCount"].append(len(j[task]["labels"]))
 
-        for target_model in target_models:
-            print(target_model)
-            for base_model in BASE_MODELS:
-                if not os.path.exists(os.path.join(base_dir, target_model, f"{base_model}-demo.json")):
-                    continue
-                with open(os.path.join(base_dir, target_model, f"{base_model}-demo.json")) as f:
-                    j = json.load(f)
-                    for task in j.keys():
-                        df["BaseModel"].append(base_model)
-                        df["TargetModel"].append(target_model)
-                        df["Task"].append(task)
-                        df["TaskDataCount"].append(len(j[task]["labels"]))
+                    df["Accuracy"].append(
+                        np.mean(np.array(j[task]["predictions"]) == np.array(j[task]["labels"]))
+                    )
 
-                        df["Accuracy"].append(
-                            np.mean(np.array(j[task]["predictions"]) == np.array(j[task]["labels"]))
+                    df["Transferability"].append(
+                        np.mean(np.array(j[task]["predictions"]) != np.array(j[task]["labels"]))
+                    )
+                    refusal_mask = np.array(j[task]["predictions"]) == -1
+                    df["RR+NE"].append(np.mean(refusal_mask))
+                    df["AccuracyNoRefusal"].append(
+                        np.mean(
+                            np.array(j[task]["predictions"])[~refusal_mask] == np.array(j[task]["labels"])[
+                                ~refusal_mask]
                         )
-
-                        df["Transferability"].append(
-                            np.mean(np.array(j[task]["predictions"]) != np.array(j[task]["labels"]))
-                        )
-                        refusal_mask = np.array(j[task]["predictions"]) == -1
-                        df["RR+NE"].append(np.mean(refusal_mask))
-                        df["AccuracyNoRefusal"].append(
-                            np.mean(
-                                np.array(j[task]["predictions"])[~refusal_mask] == np.array(j[task]["labels"])[~refusal_mask]
-                            )
-                        )
-                    refusals = {}
-                    for task in j.keys():
-                        preds = j[task]["predictions"]
-                        responses = j[task]["responses"]
-                    refusals[task] = [y["choices"][0]["message"]["content"] for x, y in zip(preds, responses) if x == -1]
-                    with open(os.path.join(base_dir, f"{target_model}/{base_model}-demo-refusal.json"), "w") as f:
-                        json.dump(refusals, f, indent=4)
-                        print(target_model, base_model)
-                        print(json.dumps(refusals, indent=4))
+                    )
+                refusals = {}
+                for task in j.keys():
+                    preds = j[task]["predictions"]
+                    responses = j[task]["responses"]
+                refusals[task] = [y["choices"][0]["message"]["content"] for x, y in zip(preds, responses) if x == -1]
+                with open(os.path.join(base_dir, f"{target_model}/{base_model}-demo-refusal.json"), "w") as f:
+                    json.dump(refusals, f, indent=4)
+                    print(target_model, base_model)
+                    print(json.dumps(refusals, indent=4))
     df = pd.DataFrame(df)
     print(df)
     df.to_csv(os.path.join(RESULT_DIR, "task_breakdown.csv"), index=False)
