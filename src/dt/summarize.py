3b99516d7d3e1cb1e67b97dec4e5cf0ee1f3968c
Chejian Xu <shudezx@gmail.com>
2024-03-10 13:56:51 -0500

    Update tutorial and check results in summarize.py

--- src/dt/summarize.py
+++ src/dt/summarize.py
@@ -32,7 +32,8 @@ def get_adv_demo_scores(breakdown=False):
 
 
 def get_advglue_scores(breakdown=False):
-    print(os.path.join(RESULT_DIR, "adv-glue-plus-plus", "summary.json"))
+    if not os.path.exists(os.path.join(RESULT_DIR, "adv-glue-plus-plus", "summary.json")):
+        return None
     with open(os.path.join(RESULT_DIR, "adv-glue-plus-plus", "summary.json")) as src:
         scores = json.load(src)
     model_scores = {k.lstrip("/"): v * 100 for k, v in scores["Accuracy"].items()}
@@ -40,9 +41,6 @@ def get_advglue_scores(breakdown=False):
     if breakdown:
         with open(os.path.join(RESULT_DIR, "adv-glue-plus-plus", "breakdown.json")) as src:
             breakdown_scores = json.load(src)
-            for k, v in breakdown_scores.items():
-                for task_name in v:
-                    breakdown_scores[k][task_name]["acc"] *= 100
             return breakdown_scores
     else:
         return {"score": model_scores, "rejection_rate": model_rejections}
@@ -79,6 +77,8 @@ def get_fairness_scores(breakdown=False):
 
 
 def get_ethics_scores(breakdown=False):
+    if not os.path.exists(os.path.join(RESULT_DIR, "machine_ethics", "generations", "scores.jsonl")):
+        return None
     df = pd.read_json(os.path.join(RESULT_DIR, "machine_ethics", "generations", "scores.jsonl"), lines=True)
     if breakdown:
         keys = ["avg_fpr_ev", "avg_fpr_jb", "acc_few", "acc_zero"]
@@ -140,6 +140,8 @@ def get_ood_scores(breakdown=False):
 
 
 def get_privacy_scores(breakdown=False):
+    if not os.path.exists(os.path.join(RESULT_DIR, "privacy", "generations", "scores.jsonl")):
+        return None
     df = pd.read_json(os.path.join(RESULT_DIR, "privacy", "generations", "scores.jsonl"), lines=True)
     # TODO: This won't work if OpenAI or Anthropic models start to have underscores
     df["model"] = df["model"].apply(lambda x: x.replace("_", "/", 2))
